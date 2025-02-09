5d0c77a79164fb7d8c2e76d2885bd0b8e72cb4d7
Eorime <elenedgebuadze83@gmail.com>
2025-02-09 21:45:21 +0400

    Move fetching logic from script to django for accessability

--- bookExchange/views.py
+++ bookExchange/views.py
@@ -5,7 +5,32 @@ from .models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db import IntegrityError
+import requests
 
+def fetch_books():
+    books = []
+    start_index = 0
+    max_results = 20
+    total_limit = 50
+    query = "book OR novel OR literature OR fiction"
+    lang = "en"
+
+    while start_index < total_limit:
+        api_url = f"https://www.googleapis.com/books/v1/volumes?q={query}&langRestrict={lang}&startIndex={start_index}&maxResults={max_results}"
+        response = requests.get(api_url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            if "items" in data:
+                books.extend(data["items"])
+            else:
+                break
+        else:
+            break  
+
+        start_index += max_results
+
+    return books
 
 # Create your views here.
 
@@ -59,4 +84,8 @@ def shelf(request):
     return render(request, "shelf.html") 
 
 def browse(request):
-    return render(request,  "browse.html")
+    books = fetch_books
+    return render(request,  "browse.html",
+   {
+       "books": books
+   } )
