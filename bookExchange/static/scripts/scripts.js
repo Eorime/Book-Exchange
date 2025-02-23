59795dda0ff9ec19c9c0f952d7d571584efb9a32
Eorime <elenedgebuadze83@gmail.com>
2025-02-23 20:19:56 +0400

    Style changes

--- bookExchange/static/scripts/scripts.js
+++ bookExchange/static/scripts/scripts.js
@@ -38,6 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		setTimeout(() => {
 			loader.style.display = "none";
 			mainContent.style.display = "block";
-		}, 4000);
+		}, 3000);
 	});
 });
